6ab721024ca44ac463ab71939b1fb0189fba21da
DeZoST <zosted62400@gmail.com>
2024-12-17 01:28:39 +0100

    Improved the Home Video

--- src/components/HomeVideo/HomeVideo.jsx
+++ src/components/HomeVideo/HomeVideo.jsx
@@ -11,7 +11,6 @@ function HomeVideo({ videos }) {
     const [activeVideoIndex, setActiveVideoIndex] = useState(0);
     const [isLoading, setIsLoading] = useState(true);
     const [isFadingOut, setIsFadingOut] = useState(false);
-    const [isFirstVideoLoaded, setIsFirstVideoLoaded] = useState(false);
     const videoRef = useRef(null);
     const progressRefs = useRef([]);
     const readyCheckInterval = useRef(null);
@@ -20,63 +19,60 @@ function HomeVideo({ videos }) {
 
     const handleVideoEnd = () => {
         if (!isMobile) {
-            const nextIndex = (activeVideoIndex + 1) % videos.length;
-            setActiveVideoIndex(nextIndex);
+            setActiveVideoIndex((prevIndex) => (prevIndex + 1) % videos.length);
         }
     };
 
-    const handleHover = (index) => {
-        setActiveVideoIndex(index);
-    };
+    const handleHover = (index) => setActiveVideoIndex(index);
 
     const checkIfVideoReady = () => {
         const video = videoRef.current;
         if (video && video.readyState >= 3) {
             clearInterval(readyCheckInterval.current);
-
-            setIsFadingOut(true);
-
-            setTimeout(() => {
-                setIsLoading(false);
-                const event = new CustomEvent("pageReady");
-                window.dispatchEvent(event);
-            }, 500);
+            fadeOutLoader();
         }
+    };
+
+    const fadeOutLoader = () => {
+        setIsFadingOut(true);
+        setTimeout(() => {
+            setIsLoading(false);
+            window.dispatchEvent(new CustomEvent("pageReady"));
+        }, 500);
     };
 
     const updateProgress = () => {
         const video = videoRef.current;
-        const progressBar = progressRefs.current[activeVideoIndex];
-        if (video && progressBar) {
+        if (video && progressRefs.current[activeVideoIndex]) {
             const progress = (video.currentTime / video.duration) * 100 || 0;
-            progressBar.style.width = `${progress}%`;
+            progressRefs.current[activeVideoIndex].style.width = `${progress}%`;
         }
     };
 
-    const handleFirstVideoLoad = () => {
-        setIsFirstVideoLoaded(true);
+    const initializeVideoPlayback = () => {
+        const video = videoRef.current;
+        if (video) {
+            video
+                .play()
+                .catch((err) => console.warn("Play method failed: ", err));
+            readyCheckInterval.current = setInterval(checkIfVideoReady, 100);
+            video.addEventListener("timeupdate", updateProgress);
+        }
+    };
+
+    const clearVideoEventListeners = () => {
+        const video = videoRef.current;
+        if (video) {
+            video.removeEventListener("timeupdate", updateProgress);
+        }
+        clearInterval(readyCheckInterval.current);
     };
 
     useEffect(() => {
-        if (videoRef.current) {
-            videoRef.current
-                .play()
-                .then(() => console.log("Video play triggered."))
-                .catch((err) => console.warn("Play method failed: ", err));
-
-            readyCheckInterval.current = setInterval(checkIfVideoReady, 100);
-
-            videoRef.current.addEventListener("timeupdate", updateProgress);
-        }
+        initializeVideoPlayback();
 
         return () => {
-            clearInterval(readyCheckInterval.current);
-            if (videoRef.current) {
-                videoRef.current.removeEventListener(
-                    "timeupdate",
-                    updateProgress
-                );
-            }
+            clearVideoEventListeners();
         };
     }, [activeVideoIndex]);
 
@@ -86,78 +82,72 @@ function HomeVideo({ videos }) {
         });
     }, [activeVideoIndex]);
 
-    useEffect(() => {
-        if (isFirstVideoLoaded) {
-            setIsLoading(false);
-        }
-    }, [isFirstVideoLoaded]);
+    const renderMobileView = () => (
+        <ul>
+            {videos.map((video, index) => (
+                <li
+                    key={index}
+                    className={`relative flex items-end w-full h-dvh ${revealClass}`}
+                >
+                    <figure className="flex items-end w-full h-full">
+                        <video
+                            ref={index === 0 ? videoRef : null}
+                            src={video.src}
+                            preload="auto"
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="absolute top-0 left-0 object-cover w-full h-dvh"
+                            onLoadedData={fadeOutLoader}
+                        />
+                        <figcaption className="z-0 flex items-end justify-between w-full p-4 text-xs tracking-tight">
+                            <div>
+                                <p className="font-semibold text-gray-300">
+                                    {video.title}
+                                </p>
+                                <p className="text-gray-400">{video.brand}</p>
+                            </div>
+                            <p className="text-gray-400">
+                                Director: {video.director}
+                            </p>
+                        </figcaption>
+                    </figure>
+                </li>
+            ))}
+        </ul>
+    );
+
+    const renderDesktopView = () => (
+        <>
+            <figure className="absolute top-0 left-0 w-full h-full">
+                <video
+                    ref={videoRef}
+                    src={videos[activeVideoIndex].src}
+                    preload="auto"
+                    autoPlay
+                    loop={false}
+                    muted
+                    playsInline
+                    className="object-cover w-full h-full"
+                    onEnded={handleVideoEnd}
+                    onLoadedData={fadeOutLoader}
+                />
+            </figure>
+            <HomeVideoDetails
+                videos={videos}
+                activeVideoIndex={activeVideoIndex}
+                handleHover={handleHover}
+                progressRefs={progressRefs}
+                revealClass={revealClass}
+            />
+        </>
+    );
 
     return (
         <section className="relative w-full h-screen">
             {isLoading && <Loader fadeOut={isFadingOut} />}
-
-            {isMobile ? (
-                <ul>
-                    {videos.map((video, index) => (
-                        <li
-                            key={index}
-                            className={`relative flex items-end w-full h-dvh ${revealClass} `}
-                        >
-                            <figure className="flex items-end w-full h-full">
-                                <video
-                                    ref={index === 0 ? videoRef : null}
-                                    src={video.src}
-                                    preload="auto"
-                                    autoPlay
-                                    loop
-                                    muted
-                                    playsInline
-                                    className="absolute top-0 left-0 object-cover w-full h-dvh"
-                                    onLoadedData={handleFirstVideoLoad}
-                                />
-                                <figcaption className="z-0 flex items-end justify-between w-full p-4 text-xs tracking-tight ">
-                                    <div>
-                                        <p className="font-semibold text-gray-300">
-                                            {video.title}
-                                        </p>
-                                        <p className="text-gray-400">
-                                            {video.brand}
-                                        </p>
-                                    </div>
-                                    <p className="text-gray-400">
-                                        Director: {video.director}
-                                    </p>
-                                </figcaption>
-                            </figure>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <>
-                    <figure className={`absolute top-0 left-0 w-full h-full`}>
-                        <video
-                            ref={videoRef}
-                            src={videos[activeVideoIndex].src}
-                            preload="auto"
-                            autoPlay
-                            loop={false}
-                            muted
-                            playsInline
-                            className="object-cover w-full h-full"
-                            onEnded={handleVideoEnd}
-                            onLoadedData={handleFirstVideoLoad}
-                        />
-                    </figure>
-
-                    <HomeVideoDetails
-                        videos={videos}
-                        activeVideoIndex={activeVideoIndex}
-                        handleHover={handleHover}
-                        progressRefs={progressRefs}
-                        revealClass={revealClass}
-                    />
-                </>
-            )}
+            {isMobile ? renderMobileView() : renderDesktopView()}
         </section>
     );
 }
