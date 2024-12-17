30bcb6393f343366965e098a56f3298e58c90278
DeZoST <zosted62400@gmail.com>
2024-12-17 01:29:21 +0100

    Cache change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,10 @@ import HomeVideo from "@/components/HomeVideo/HomeVideo";
 export const revalidate = 3600;
 
 export default async function Home() {
-    const res = await fetch(
-        `https://electra-website-dusky.vercel.app/data/home_video.json`
-    );
-    const videos = await res.json();
+    const videos = await fetch(
+        `https://electra-website-dusky.vercel.app/data/home_video.json`,
+        { next: { revalidate: 3600 } }
+    ).then((res) => res.json());
 
     return <HomeVideo videos={videos} />;
 }
