dc2ef296e8c4b1df9370181f947c5e48827235ea
DeZoST <zosted62400@gmail.com>
2025-01-28 06:14:09 +0100

    Added Suspense fallback

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,11 +1,12 @@
+import { Suspense } from "react";
 import Header from "../Header/Header";
 
 function Layout({ children }) {
     return (
-        <>
+        <Suspense fallback={<div>Loading...</div>}>
             <Header />
             <main>{children}</main>
-        </>
+        </Suspense>
     );
 }
 
