cc2d80a1a6c77dd02b62ec8624bb23b5ea98b0d5
DeZoST <zosted62400@gmail.com>
2025-01-28 05:12:16 +0100

    Made the fetching logic server side by fetching it in the page directly with NextJS recommended way

--- src/app/narrative/page.jsx
+++ src/app/narrative/page.jsx
@@ -1,17 +1,23 @@
-"use client";
-
 import ProjectList from "@/components/ProjectList/ProjectList";
+import fetchPaginatedData from "@/utils/fetchPaginatedData";
 
-export default function Narrative() {
+export default async function Narrative() {
     const jsonUrl =
         "https://electra-website-dusky.vercel.app/data/narrative.json";
+    let projects = [];
+
+    try {
+        projects = await fetchPaginatedData(jsonUrl);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 
     return (
         <div className="p-4 mt-32 lg:p-8 lg:mt-60">
             <h1 className="text-xl font-bold text-orange-400 uppercase md:text-3xl">
                 Narrative
             </h1>
-            <ProjectList jsonUrl={jsonUrl} />
+            <ProjectList projects={projects} />
         </div>
     );
 }
