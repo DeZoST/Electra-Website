fc53c438fb168b322a92ec1e07b82b15ae1347d8
DeZoST <zosted62400@gmail.com>
2025-02-16 06:23:45 +0100

    Centered the Loading text

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -3,7 +3,13 @@ import Header from "@/components/Header/Header";
 
 function Layout({ children }) {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+            fallback={
+                <div className="flex items-center justify-center w-full h-dvh">
+                    Loading...
+                </div>
+            }
+        >
             <Header />
             <main>{children}</main>
         </Suspense>
